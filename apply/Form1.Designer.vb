c33b1b1dd9edad9308bc7c7d950c650c4240081d
zhaobin <zhaobin@ZHAOBIN-PC>
2017-11-15 16:24:12 +0800

    添加项目文件。

---
+++ apply/Form1.Designer.vb
@@ -0,0 +1,102 @@
+﻿
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(3, 3)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(325, 39)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "读取"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.Location = New System.Drawing.Point(334, 3)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(326, 39)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "写入"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.Button1, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.RichTextBox1, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button2, 1, 0)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.46512!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 89.53488!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(663, 430)
+        Me.TableLayoutPanel1.TabIndex = 4
+        '
+        'RichTextBox1
+        '
+        Me.TableLayoutPanel1.SetColumnSpan(Me.RichTextBox1, 2)
+        Me.RichTextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.RichTextBox1.Location = New System.Drawing.Point(3, 48)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(657, 379)
+        Me.RichTextBox1.TabIndex = 3
+        Me.RichTextBox1.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(663, 430)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents RichTextBox1 As RichTextBox
+End Class
---
+++ apply/Form1.vb
@@ -0,0 +1,79 @@
+﻿
+Public Class Form1
+    Private oWordApplication As Microsoft.Office.Interop.Word.Application
+    Private oDocument As Microsoft.Office.Interop.Word.Document
+    Private oRange As Microsoft.Office.Interop.Word.Range
+    Private oSelection As Microsoft.Office.Interop.Word.Selection
+    Private path
+    Private newfilename
+    'Public Sub New()
+    '    '激活word接口
+    '    oWordApplication = New Microsoft.Office.Interop.Word.Application
+    '    oWordApplication.Visible = True
+    'End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Dia As Object
+        Dia = New OpenFileDialog
+        Dia.title = "xxx"
+        Dia.filter = "所有文件(*.*)|*.*|所有文件(*.*)|*.*"
+        Dia.filterindex = 1
+        Dia.restoredirectory = False
+        If (Dia.showdialog() = DialogResult.OK) Then
+            path = Dia.filename
+        End If
+        OpenFile(path)
+        RichTextBox1.Text = oDocument.Range.Text
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim Dia As Object
+        Dia = New SaveFileDialog
+        Dia.title = "xxx"
+        Dia.filter = "所有文件(*.*)|*.*|所有文件(*.*)|*.*"
+        Dia.filterindex = 1
+        Dia.filename = "文件未命名"
+        Dia.restoredirectory = False
+        If (Dia.showdialog() = DialogResult.OK) Then
+            If Dia.CheckFileExists Then
+                newfilename = Dia.filename + ".docx"
+            Else
+                newfilename = Dia.filename
+            End If
+            Me.NewDocWithModel(newfilename)
+            End If
+    End Sub
+    '创建新文档
+    Public Sub NewDocument()
+        Dim missing = System.Reflection.Missing.Value
+        Dim isVisible As Boolean = True
+        oDocument = oWordApplication.Documents.Add(missing, missing, missing, missing)
+        oDocument.Activate()
+    End Sub
+    '新建模板文档
+    Public Sub NewDocWithModel(ByVal FileName As String)
+        Dim missing = System.Reflection.Missing.Value
+        Dim isVisible As Boolean = True
+        Dim strName As String
+        strName = FileName
+        oWordApplication = New Microsoft.Office.Interop.Word.Application
+
+        oDocument = oWordApplication.Documents.Add(strName, missing, missing, isVisible)
+        oDocument.Content.InsertAfter(RichTextBox1.Text)
+
+        oDocument.Activate()
+    End Sub
+    '打开word文件
+    Public Sub OpenFile(ByVal FileName As String)
+        Dim strName As String
+        Dim isReadOnly As Boolean
+        Dim isVisible As Boolean
+        Dim missing = System.Reflection.Missing.Value
+        oWordApplication = New Microsoft.Office.Interop.Word.Application
+        strName = FileName
+        isReadOnly = False
+        isVisible = True
+        oDocument = oWordApplication.Documents.Open(strName, missing, isReadOnly, missing, missing, missing, missing, missing, missing, missing, missing, isVisible, missing, missing, missing, missing)
+        oDocument.Activate()
+    End Sub
+End Class
